0299907768899ee26402806d2232600bf954161b
Christopher Woolum <christopher.woolum@gmail.com>
2021-12-11 13:07:59 -0800

    Add serialization extensions

---
+++ src/Turnoutt.Azure.ServiceBus.Core/Extensions/SerializationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+namespace System
+{
+    public static class SerializationsExtensions
+    {
+        public static T FromByteArray<T>(this byte[] byteArray) where T : class
+        {
+            if (byteArray == null)
+            {
+                return default;
+            }
+
+            return JsonSerializer.Deserialize<T>(new ReadOnlySpan<byte>(byteArray));
+        }
+
+        public static byte[] ToByteArray(this object obj)
+        {
+            if (obj == null)
+            {
+                return null;
+            }
+
+            return JsonSerializer.SerializeToUtf8Bytes(obj,
+                    new JsonSerializerOptions { WriteIndented = false, IgnoreNullValues = true });
+        }
+    }
+}
